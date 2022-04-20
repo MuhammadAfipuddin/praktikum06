1a783808dcddbb7b937f92cc574e3f22efa4f769
muhammad afipuddin <apip398@gmail.com>
2022-04-20 15:04:36 +0700

    Pengumpulan praktikum 06

---
+++ praktek/application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Selamat Datang</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>SEMANGAT BELAJAR</h1>
+
+	<div id="body">
+		<p>JANGAN LUPA BERDOA SEBELUM BELAJAR</p>
+	</div>
+</div>
+
+</body>
+</html>
